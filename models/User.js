923a7fad42cc440792075ec74263270a898ddfa9
Peter Song <psong562@gmnail.com>
2022-01-17 16:01:11 -0800

    created models/routes

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose')
+
+const User = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must be Valid Email adress'],
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'post'
+  }],
+  notes: [{
+    type: Schema.Types.ObjectId,
+    ref: 'note'
+  }],
+  friends: [{
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  }],
+}, {toJSON: {
+    virtuals: true
+  },
+  id:false 
+},
+  { timestamps: true })
+
+
+User.virtual('friendCount').get(function () {
+  return this.friends.length
+})
+
+
+User.plugin(require('passport-local-mongoose'))
+
+module.exports = model('user', User)
